620f535030b2742e2a69cb15a0db20f74d1b8c99
Osmar Neto <osmar.neto23@gmail.com>
2024-01-19 21:22:49 -0300

    Add:NavBar

--- assets/Scripts/index.js
+++ assets/Scripts/index.js
@@ -1,1 +1,20 @@
+const menu = document.querySelector('#menu');
+const btOpenMenu = document.querySelector('#menu__open');
+const btCloseMenu = document.querySelector('#menu__close');
+const menuFade = document.querySelector('#menu__fade');
 
+function toogleMenuInactive() {
+    menu.classList.toggle('inactive');
+}
+
+btOpenMenu.addEventListener('click', toogleMenuInactive);
+
+btCloseMenu.addEventListener('click', toogleMenuInactive);
+
+
+// let nav = document.querySelector("nav");
+// let hamburger = document.querySelector(".menu-icon");
+
+// hamburger.addEventListener("click", function () {
+//     nav.classList.toggle("active");
+// });
