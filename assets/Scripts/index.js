5253584d95e4d150c0357e4bd7791fe2ebbbcb0a
Osmar Neto <osmar.neto23@gmail.com>
2024-01-20 01:18:19 -0300

    Add:Main content

--- assets/Scripts/index.js
+++ assets/Scripts/index.js
@@ -1,20 +1,24 @@
 const menu = document.querySelector('#menu');
 const btOpenMenu = document.querySelector('#menu__open');
 const btCloseMenu = document.querySelector('#menu__close');
-const menuFade = document.querySelector('#menu__fade');
+const menuFade = document.querySelector('.fade');
+const menuItens = document.querySelectorAll('.menu__item a');
 
 function toogleMenuInactive() {
     menu.classList.toggle('inactive');
+    menuFade.classList.toggle('inactive');
+}
+
+function closeMenuOnItemClick() {
+    if (!menuFade.classList.contains('inactive')) {
+        toogleMenuInactive();
+    }
 }
 
 btOpenMenu.addEventListener('click', toogleMenuInactive);
 
 btCloseMenu.addEventListener('click', toogleMenuInactive);
 
-
-// let nav = document.querySelector("nav");
-// let hamburger = document.querySelector(".menu-icon");
-
-// hamburger.addEventListener("click", function () {
-//     nav.classList.toggle("active");
-// });
+menuItens.forEach(function (item) {
+    item.addEventListener('click', closeMenuOnItemClick);
+});
